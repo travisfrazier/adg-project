472e4831301d51c9e9ee1af348740a829fa73bd9
Travis Frazier <31898316+travisfrazier@users.noreply.github.com>
2019-01-29 13:40:27 -0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// Photo Selection Function
+document.querySelector('.images').addEventListener('click', function(event) {
+	event.preventDefault();
+
+	const imageShow = document.querySelector('.shown-image');
+	imageShow.style.backgroundImage = `url(${event.target.src}`;
+	console.log(event.target);
+});
+
+// Item Selection Function
+const allProducts = document.querySelectorAll('.product');
+
+allProducts.forEach(function(product) {
+	product.addEventListener('click', function(event) {
+		event.preventDefault();
+
+		if (event.target.tagName === 'BUTTON') {
+			product.classList.toggle('selected');
+		}
+	});
+});
